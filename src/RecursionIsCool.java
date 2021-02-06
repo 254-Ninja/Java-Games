b5d961192e1d4d123f062bf100fd1b7106c9ba9e
254-ninja <muthomidenis62@gmail.com>
2021-02-06 11:14:38 +0300

    Recursion

--- src/RecursionIsCool.java
+++ src/RecursionIsCool.java
@@ -2,11 +2,16 @@
 public class RecursionIsCool {
 
 	public static void main(String[] args) {
-	sayHi();
+	sayHi(5);
 		
 	}
-	public static void sayHi() {
-		System.out.println("hi");
+	public static void sayHi(int n) {
+		if(n == 0) {
+			System.out.println("done!");
+		}
+	//	System.out.println("hi");
+	//	sayHi();
 	}
+	//Base case
 
 }
