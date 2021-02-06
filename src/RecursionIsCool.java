d7ceb70c7513a9f0e248f687aebeb8c2ae654289
254-ninja <muthomidenis62@gmail.com>
2021-02-06 11:09:41 +0300

    Recursion

--- src/RecursionIsCool.java
+++ src/RecursionIsCool.java
@@ -3,7 +3,10 @@ public class RecursionIsCool {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+	}
+	public static void sayHi() {
+		System.out.println("hi");
 	}
 
 }
