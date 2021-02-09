45d1f5b5cecb1535887efa4bd924499cfa2618e0
254-ninja <muthomidenis62@gmail.com>
2021-02-09 19:29:11 +0300

    TicTacToe

--- tic-tac-toe/src/TicTacToe.java
+++ tic-tac-toe/src/TicTacToe.java
@@ -2,8 +2,12 @@
 public class TicTacToe {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		char [][] gameBoard = {{' ', '|', ' ', '|', ' '}, 
+				{'-', '+', '-', '+', '-'},
+				{' ', '|', ' ', '|', ' '}, 
+				{'-', '+', '-', '+', '-'}, 
+				{' ', '|', ' ', '|', ' '}};
 	}
 
 }
