bd7a350372314f0f46513b2b1b94d04fd91fa512
254-ninja <muthomidenis62@gmail.com>
2021-02-10 10:05:26 +0300

    TicTacToe

--- tic-tac-toe/src/TicTacToe.java
+++ tic-tac-toe/src/TicTacToe.java
@@ -20,7 +20,39 @@ public class TicTacToe {
 		switch(pos) {
 		   case 1:
 			   gameBoard[0][0] = 'X';
-			
+			   break;
+		   
+		   case 2:
+			   gameBoard[0][2] = 'X';
+			   break;
+			   
+		   case 3:
+			   gameBoard[0][4] = 'X';
+			   break;
+			   
+		   case 4:
+			   gameBoard[2][0] = 'X';
+			   break;
+			   
+		   case 5:
+			   gameBoard[2][2] = 'X';
+			   break;
+			   
+		   case 6:
+			   gameBoard[2][4] = 'X';
+			   break;
+			   
+		   case 7:
+			   gameBoard[4][0] = 'X';
+			   break;
+			   
+		   case 8:
+			   gameBoard[4][2] = 'X';
+			   break;
+			   
+		   case 9:
+			   gameBoard[4][4] = 'X';
+			   break;
 		}
 	}
 	
