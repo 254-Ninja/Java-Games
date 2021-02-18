66f9bf937cf42eaa18f6a0b00b9325294aef0a3b
254-ninja <muthomidenis62@gmail.com>
2021-02-18 08:47:55 +0300

    Check winner

--- tic-tac-toe/src/TicTacToe.java
+++ tic-tac-toe/src/TicTacToe.java
@@ -101,6 +101,13 @@ public class TicTacToe {
 	public static String checkWinner() {
 		
 		List topRow = Arrays.asList(1, 2, 3);
+		List midRow = Arrays.asList(4, 5, 6);
+		List botRow = Arrays.asList(7, 8, 9);
+		List leftCol = Arrays.asList(1, 4, 7);
+		List midCol = Arrays.asList(2, 5, 8);
+		List rightCol = Arrays.asList(3, 6, 9);
+		List cross1 = Arrays.asList(1, 5, 9);
+		List cross2 = Arrays.asList(7, 5, 3);
 		
 		return "";
 		
