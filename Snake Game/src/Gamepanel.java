c8ce2e84a3da243affd2060a79e817c1386841e5
254-ninja <muthomidenis62@gmail.com>
2021-02-19 20:46:30 +0300

    Game Panel

--- Snake Game/src/Gamepanel.java
+++ Snake Game/src/Gamepanel.java
@@ -2,6 +2,11 @@ import javax.swing.JPanel;
 
 public class Gamepanel extends JPanel{
 	
+	
+	private static final long serialVersionUID = 1L;
+	
+	public static final int WIDTH = 500, HEIGHT = 500;
+
 	public Gamepanel() {
 		
 	}
--- Snake Game/src/Main.java
+++ Snake Game/src/Main.java
@@ -5,7 +5,9 @@ public class Main {
 	public Main() {
 		
 		JFrame frame = new JFrame();
+		Gamepanel gamepanel = new Gamepanel();
 		
+		frame.add(gamepanel);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setTitle("SNAKECODING");
 		frame.setLocationRelativeTo(null);
