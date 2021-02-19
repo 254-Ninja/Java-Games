82ada9336339199a9fcafc14609b5cb98af90d54
254-ninja <muthomidenis62@gmail.com>
2021-02-19 21:00:48 +0300

    Game Panel

--- Snake Game/src/Gamepanel.java
+++ Snake Game/src/Gamepanel.java
@@ -1,18 +1,38 @@
 import java.awt.Dimension;
+import java.awt.Graphics;
 
 import javax.swing.JPanel;
 
-public class Gamepanel extends JPanel{
+public class Gamepanel extends JPanel implements Runnable{
 	
 	
 	private static final long serialVersionUID = 1L;
 	
 	public static final int WIDTH = 500, HEIGHT = 500;
+	
 
 	public Gamepanel() {
 		
 		setPreferredSize(new Dimension(WIDTH, HEIGHT));
+				
+	}
+	
+	public void start() {		
 		
+	}
+	public void stop() {
+		
+	}
+	public void tick() {
+		
+	}
+	public void point(Graphics g) {
+		
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
 		
 	}
 
