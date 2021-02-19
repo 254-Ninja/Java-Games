233fc539db43c13e5d08f2e38ee652420a120210
254-ninja <muthomidenis62@gmail.com>
2021-02-19 20:34:47 +0300

    Snake Game

--- Snake Game/src/Main.java
+++ Snake Game/src/Main.java
@@ -5,6 +5,13 @@ public class Main {
 	public Main() {
 		
 		JFrame frame = new JFrame();
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setTitle("SNAKECODING");
+		frame.setLocationRelativeTo(null);
+		
+		frame.pack();
+		frame.setVisible(true);
 	}
 
 	public static void main(String[] args) {
