c48fefacd72d529b439cdc73db66c5e3a0d63aba
254-ninja <muthomidenis62@gmail.com>
2021-02-20 11:04:22 +0300

    Game Panel

--- Snake Game/src/Gamepanel.java
+++ Snake Game/src/Gamepanel.java
@@ -1,3 +1,4 @@
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 
@@ -28,9 +29,21 @@ public class Gamepanel extends JPanel implements Runnable{
 	}
 	public void point(Graphics g) {
 		
+		g.clearRect(0, 0, WIDTH, HEIGHT);
+        g.setColor(Color.ORANGE);
+        g.fillRect(0, 0, WIDTH, HEIGHT);
+        
+        g.setColor(Color.BLACK);
+		
 		for (int i = 0; i < WIDTH / 10; i++) {
             g.drawLine(i * 10, 0, i * 10, HEIGHT);
         }
+		
+		 for (int i = 0; i < HEIGHT / 10; i++) {
+	            g.drawLine(0, i * 10, WIDTH, i * 10);
+	        }
+	 
+	        
 		
 	}
 
